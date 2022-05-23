10bf0ae30f134f8fee42237f9115224b59b39512
HamidZahid <hamidchaudhary3867@gmail.com>
2022-05-23 20:14:21 +0500

    Modal Added

--- CompleteTask.php
+++ CompleteTask.php
@@ -51,21 +51,65 @@
         <!-- modal  -->
         <!-- Button trigger modal -->
         <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal fade " id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <h5 class="modal-title" id="exampleModalLabel">Add New Task</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
                     <div class="modal-body">
-                        ...
+                    <form>
+            <div class="form-row">
+              <div class="form-group col-md-6">
+                <label for="inputEmail4">Task Title</label>
+                <input type="text" class="form-control" id="" value="" data-id="" placeholder="Text"/>
+              </div>
+              <div class="form-group col-md-6">
+                <label for="inputPassword4">Assign To: </label>
+                <select class="form-control" id="" data-id="" value="">
+                    <option>Select Employee</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                  </select>
+              </div>
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                  <label for="inputEmail4">Form:</label>
+                  <input type="date" class="form-control" id="" value="" data-id="" placeholder=""/>
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="inputPassword4">To:</label>
+                  <input type="date" class="form-control" id="" value="" data-id="" placeholder=""/>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="inputAddress">Attachment:</label>
+                <input type="file" class="form-control" id="" value="" data-id="" placeholder="" style="border: none;"/>
+              </div>
+              <label for="inputAddress">Work Status:</label>
+              <div class="custom-control custom-radio custom-control-inline mx-3">
+                <input type="radio" id="customRadioInline1" name="customRadioInline" class="custom-control-input">
+                <label class="custom-control-label" for="customRadioInline1">Completed</label>
+              </div>
+              <div class="custom-control custom-radio custom-control-inline">
+                <input type="radio" id="customRadioInline2" name="customRadioInline" class="custom-control-input">
+                <label class="custom-control-label" for="customRadioInline2">Pending</label>
+              </div>
+              <div class="custom-control custom-radio custom-control-inline">
+                <input type="radio" id="customRadioInline3" name="customRadioInline" class="custom-control-input">
+                <label class="custom-control-label" for="customRadioInline3">On Progress</label>
+              </div>
+          </form>
                     </div>
                     <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
+                        <button type="button" class="btn btn-outline-primary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-success">Add</button>
                     </div>
                 </div>
             </div>
